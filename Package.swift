dbbbc22a5db2e676c45679da53dd3d859b44e3bc
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-04-28 20:05:54 +0200

    revert

--- Package.swift
+++ Package.swift
@@ -3,5 +3,5 @@ import PackageDescription
 let package = Package(
 	name: "COpenSSL",
 	pkgConfig: "openssl",
-	providers: [.Brew("openssl"), .Apt("libssl")]
+	providers: [.Brew("openssl"), .Apt("openssl")]
 )
